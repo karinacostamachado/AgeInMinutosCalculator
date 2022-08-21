a7e67b88e7f08bf2834d6580a7ccf4bd8098d7f8
karinacostaconceicao <karina.cmc@outlook.com>
2022-08-21 20:41:52 -0300

    results class deleted

--- app/src/main/java/com/example/agecalculator/model/Results.kt
+++
@@ -1,7 +0,0 @@
-package com.example.agecalculator.model
-
-sealed class Results<T>(val data: T? = null, val message: String? = null){
-    class Success<T>(data: T?): Results<T>(data)
-    class Error<T>(message: String?, data: T? = null): Results<T>(data)
-    class Loading<T>(data: T? = null): Results<T>(data)
-}
